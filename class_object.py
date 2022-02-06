13afe9e76c1b990b0bbf0402bf0e9418ba859cf7
bhargav-io <bhargavdas77@hotmail.com>
2022-02-06 13:27:14 +0530

    Test2

---
+++ class_object.py
@@ -0,0 +1,10 @@
+#  A class is like a template and an object is the actual entity
+# We create the template and then define the template in the object
+
+class iphone_12:
+
+    def __init__(self, brand, storage, price,screen_size):
+        self.brand = brand
+        self.storage = storage
+        self.price = price
+        self.screen_size = screen_size
---
+++ object_class.py
@@ -0,0 +1,10 @@
+from class_object import iphone_12
+
+iphone_12_64 = iphone_12("Apple",64,58.999,6.1)
+
+iphone_12_128 = iphone_12("Apple",128, 65.999,6.1)
+
+print(iphone_12_128.price)
+
+
+#This is the object file, where we define the template itself
